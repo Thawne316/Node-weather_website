31654238b7feec0772ad749cee18ea7bd61a370f
Rohith <rohithv355@gmail.com>
2020-05-05 15:25:07 +0530

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,22 @@
+console.log('Client side js is loaded')
+
+const weatherform = document.querySelector('form')
+const search = document.querySelector('input')
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+weatherform.addEventListener('submit', (e) => {
+    e.preventDefault()
+    message1.textContent = 'Loading..'
+    const location = search.value
+    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    response.json().then((data) => {
+        if(data.error) {
+            message1.textContent = data.error
+        } else {
+            message1.textContent = data.location
+            message2.textContent = data.weather
+        }  
+    })    
+})
+})
---
+++ src/app.js
@@ -0,0 +1,101 @@
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode =  require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+console.log(__dirname)
+console.log(path.join(__dirname,'../public'))
+const app = express()
+
+//Define paths for Express config
+const publicDirectoryPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+//Setup handlebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
+//Setup static directory to use
+app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title:'Weather',
+        name: 'Rohith'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        name: 'Rohith',
+        title: 'About me'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help page',
+        para: 'This is the help page',
+        name: 'Rohith'
+    })
+})
+
+app.get('/weather', (req,res) => {
+    if (!req.query.address){
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    const address = req.query.address
+    geocode(address, (error,{lat, long, location} = {}) => {
+        if(error) {
+          return res.send({
+              error
+          })
+        } 
+        forecast(lat, long, (error, forecastData) => {
+          if(error) {
+            return res.send({
+                error
+            })
+          }
+          
+          res.send({
+            weather: forecastData,
+            location,
+            address: req.query.address
+        })
+        })
+      })
+    
+})
+
+app.get('/products', (req, res) => {
+    if (!req.query.search){
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404page', {
+        message: 'Help article not found'
+    })
+})
+
+app.get('*', (req, res) => {
+    res.render('404page', {
+        message:'Page not found'
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000')
+})
